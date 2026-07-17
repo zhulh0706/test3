9a02d045d2fcf663cf820982f09a83cc95f0d518
zhulh <1053831559@qq.com>
2026-07-17 18:50:34 +0800

    f

--- src/A.java
+++ src/A.java
@@ -1,7 +1,5 @@
 public class A {
     public static void main(String[] args) {
         System.out.print("test test test test test");
-        System.out.print("test test test test test1");
-        System.out.print("test test test test test1");
-    }
+           }
 }
